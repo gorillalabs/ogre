c0af5ab209268be6d28fb463f7db554bddb520e4
Ben Hartmann <bh@cypp.hamburg>
2016-08-26 14:20:23 +0200

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/ogre "3.0.3.1-SNAPSHOT"
+(defproject gorillalabs/ogre "3.0.3.1"
 
   :description "Clojure dialect of the Gremlin graph processing language"
   :url "https://github.com/clojurewerkz/ogre"
