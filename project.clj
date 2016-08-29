92401f2ceb3e514664d1db8c30db440eb1382c3c
Bjoern Petri <bpetri@gmx.net>
2016-08-29 15:55:14 +0200

    Release 3.0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/ogre "3.0.3.0"
+(defproject gorillalabs/ogre "3.0.4.0"
   :description "Clojure dialect of the Gremlin graph processing language"
   :url "https://github.com/clojurewerkz/ogre"
   :license {:name "Eclipse Public License"
